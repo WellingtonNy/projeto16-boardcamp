28a9a4012d0d05c8ee56f3c40f62bf5601b1db72
WellingtonLinux <wellingtoncpfilho@gmail.com>
2023-05-15 17:01:31 -0300

    tentativa joi

--- src/schemas/games.schemas.js
+++ src/schemas/games.schemas.js
@@ -3,6 +3,6 @@ import joi from "joi"
 export const gamesSchema = joi.object({
 name: joi.string().required(),
 image: joi.string().required(),
-stockTotal: joi.string().min(1).required(),
-pricePerDay: joi.string().min(1).required(),
+stockTotal: joi.number().min(1).required(),
+pricePerDay: joi.number().min(1).required(),
 })
