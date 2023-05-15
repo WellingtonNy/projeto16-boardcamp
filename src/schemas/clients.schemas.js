ffc917e2ec3df1af74aa873cd3c7964e821daddc
WellingtonLinux <wellingtoncpfilho@gmail.com>
2023-05-15 16:30:39 -0300

    joi

--- src/schemas/clients.schemas.js
+++ src/schemas/clients.schemas.js
@@ -3,7 +3,7 @@ import joi from "joi"
 export const clientSchema = joi.object({
     name: joi.string().required(),
     phone: joi.number().min(10).max(11).required(),
-    cpf: joi.number().length(11).required(),
+    cpf: joi.number().min(11).max(11).required(),
     birthday: joi.date().required(),
     })
     
